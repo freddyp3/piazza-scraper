4eb666d233e799a994c9f74a9c1c9ba45e800a09
Fredrik Pettit <fredrikpettit@gmail.com>
2026-05-23 20:36:47 -0700

    scraping logic for text only posts

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Piazza Scraper
 
-python script to scrape all questions on piazza.
+python script to scrape all posts on piazza and convert into a pretty markdown format.
--- scrape.py
+++ scrape.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 from piazza_api import Piazza
 from pprint import pprint
+from markdownify import markdownify as md
 import os
 import json
 
@@ -11,6 +12,84 @@ def load_config():
     network_id = os.getenv("PIAZZA_NETWORK_ID_CPSC310_2025W2")
     return (email, password, network_id)
 
+def html_to_md(html):
+    if html is None:
+        return ""
+    
+    return md(html)
+
+def format_post(post):
+    output = ""
+
+    post_title = html_to_md(post["history"][-1]["subject"])
+    post_content = html_to_md(post["history"][-1]["content"])
+    tags = post["tags"]
+    type_of_post = normalize_type_of_post(post["type"])
+
+    # YAML frontmatter
+    output += "---\n"
+    output += "post_number: " + str(post["nr"]) + "\n"
+    output += "title: \"" + post_title.strip() + "\"\n"
+    output += "type: " + post["type"] + "\n"
+    output += "date: " + post["created"][:10] + "\n"
+    output += "folders: " + json.dumps(post.get("folders", [])) + "\n"
+    output += "tags: " + json.dumps(tags) + "\n"
+    output += "---\n\n"
+
+    output += post_title + "\n" + post_content + "\n" + type_of_post + " | Tags: " + ", ".join(tags)    
+    children = format_children_helper(post["children"])
+    
+    output += children
+
+    return output
+
+def normalize_type_of_post(type):
+    if type == "question":
+        return "Question"
+    elif type == "note":
+        return "Note"
+    
+    return "unidentified type of post: " + type
+
+def normalize_response(response):
+    if response == "s_answer":
+        return "Student Answer"
+    elif response ==  "i_answer":
+        return "Instructor Answer"
+    elif response == "followup":
+        return "Follow Up"
+    elif response == "feedback":
+        return "Reply to Follow Up"
+    
+    return "unidentified response: " + response
+
+def format_children_helper(children):
+    output = ""
+
+    while children:
+        current = children.pop(0)
+        type_of_response = current["type"]
+        response = ""
+        if type_of_response == "s_answer" or type_of_response == "i_answer":
+            response = html_to_md(current["history"][-1]["content"])
+        else:
+            response = html_to_md(current["subject"])
+
+        if current["children"]:
+            children = current["children"] + children
+
+        normalized_response = normalize_response(type_of_response)
+
+        output += "\n" + normalized_response + ": " + response
+
+    return output
+
+
+
+
+#    with open("sample.md", "w") as f:
+#        json.dump(<whatever>, f, indent=2)
+
 if __name__ == "__main__":
     email, password, network_id = load_config()
 
@@ -18,6 +97,24 @@ if __name__ == "__main__":
     p.user_login(email, password)
     network = p.network(network_id)
 
-    post = network.get_post(6)
-    with open("sample.json", "w") as f:
-        json.dump(post, f, indent=2)
+    # default post (i_answer, s_answer, followup, feedback)
+    post = network.get_post(11)
+
+    # post with image
+    #post = network.get_post(11)
+
+    # code to write to file
+    #with open("sample_with_img.json", "w") as f:
+        #json.dump(post, f, indent=2)
+
+    pprint(format_post(post))
+
+
+    
+
+
+    
+
+
+
+
